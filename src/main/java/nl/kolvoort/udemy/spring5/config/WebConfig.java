ee02b8731ae10945c1083496502008a986936626
hkstwk <hkolvoort+github@gmail.com>
2020-10-17 19:59:02 +0200

    Udemy Spring5 - Lesson 54 - Setup Dispatcher Servlet

---
+++ src/main/java/nl/kolvoort/udemy/spring5/config/WebConfig.java
@@ -0,0 +1,11 @@
+package nl.kolvoort.udemy.spring5.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "nl.kolvoort.udemy.spring5")
+public class WebConfig {
+}
