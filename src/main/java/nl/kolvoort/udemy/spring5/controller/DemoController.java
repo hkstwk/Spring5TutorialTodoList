d2145df1d9561591d990430bf0f2c088c721050b
hkstwk <hkolvoort+github@gmail.com>
2020-10-17 23:37:06 +0200

    Udemy Spring5 - Lesson 57 - Model and Model Attributes

--- src/main/java/nl/kolvoort/udemy/spring5/controller/DemoController.java
+++ src/main/java/nl/kolvoort/udemy/spring5/controller/DemoController.java
@@ -1,9 +1,13 @@
 package nl.kolvoort.udemy.spring5.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -15,10 +19,18 @@ public class DemoController {
     }
 
     // URL http://localhost:8080/todo-list/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
     @GetMapping("welcome")
-    public String welcome(){
+    public String welcome(Model model){
+        model.addAttribute("user", "Harm");
+        log.info("model = {}", model);
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return "Welcome to my demo application";
     }
 }
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -4,6 +4,7 @@
     <title>Welcome @ Todo List</title>
 </head>
 <body>
-<h1>This is my very first jsp page ever</h1>
+<h1>Hello ${user}</h1>
+<h2>${welcomeMessage}</h2>
 </body>
 </html>
