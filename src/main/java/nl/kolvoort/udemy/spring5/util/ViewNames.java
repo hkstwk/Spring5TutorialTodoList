64928dca1e551eeb904073fa33c386a53532532b
hkstwk <hkolvoort+github@gmail.com>
2020-10-18 20:35:19 +0200

    Udemy Spring5 - Lesson 65 - Creating View and Basic JSTL tags

--- src/main/java/nl/kolvoort/udemy/spring5/util/ViewNames.java
+++ src/main/java/nl/kolvoort/udemy/spring5/util/ViewNames.java
@@ -3,7 +3,7 @@ package nl.kolvoort.udemy.spring5.util;
 public final class ViewNames {
 
     // constants
-    public static final String ITEMS_LIST = "items_list";
+    public static final String ITEMS_LIST = "items";
 
     // constructor, final to avoid extending this class
     private ViewNames() {}
