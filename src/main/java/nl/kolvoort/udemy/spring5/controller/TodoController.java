b787bb47125552363d1af9c56329f3bafa2de011
hkstwk <hkolvoort+github@gmail.com>
2020-10-19 22:23:06 +0200

    Udemy Spring5 - Lesson 68 - Implementing Post Redirect Get Pattern

--- src/main/java/nl/kolvoort/udemy/spring5/controller/TodoController.java
+++ src/main/java/nl/kolvoort/udemy/spring5/controller/TodoController.java
@@ -1,5 +1,6 @@
 package nl.kolvoort.udemy.spring5.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.kolvoort.udemy.spring5.model.TodoData;
 import nl.kolvoort.udemy.spring5.model.TodoItem;
 import nl.kolvoort.udemy.spring5.service.TodoItemService;
@@ -8,10 +9,14 @@ import nl.kolvoort.udemy.spring5.util.Mappings;
 import nl.kolvoort.udemy.spring5.util.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class TodoController {
 
@@ -35,9 +40,18 @@ public class TodoController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model){
+        TodoItem todoItem = new TodoItem("","", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
     // http://localhost/todo-items/addItem
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem){
+        log.info("todoItem from form = {}", todoItem);
+        todoItemService.addItem(todoItem);
         return "redirect:/" + Mappings.ITEMS;
     }
 
